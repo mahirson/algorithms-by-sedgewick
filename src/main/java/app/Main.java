52ec472037b62c2e3b90c163eabd8f0e4607afda
Nicat Eyyublu <nicatmahirson@gmail.com>
2025-01-11 12:20:34 +0400

    done

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -8,8 +8,7 @@ import edu.princeton.cs.algs4.StdDraw;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
         int n = 20;  // number of particles (default 20)
         if (args.length == 1) {
             n = Integer.parseInt(args[0]);

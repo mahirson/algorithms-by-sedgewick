0648e8a47ab50e4aa6ef8afb1aa6b45d059044f6
Nicat Eyyublu <nicatmahirson@gmail.com>
2025-01-11 11:06:17 +0400

    initial commit

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,31 @@
+package app;
+
+import edu.princeton.cs.algs4.CollisionSystem;
+import edu.princeton.cs.algs4.Particle;
+import edu.princeton.cs.algs4.StdDraw;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        int n = 20;  // number of particles (default 20)
+        if (args.length == 1) {
+            n = Integer.parseInt(args[0]);
+        }
+
+        // enable double buffering to support animations
+        StdDraw.enableDoubleBuffering();
+
+        // create the n particles
+        Particle[] particles = new Particle[n];
+        for (int i = 0; i < n; i++) {
+            particles[i] = new Particle();
+        }
+
+        // simulate the system
+        CollisionSystem system = new CollisionSystem(particles);
+        system.simulate(Double.POSITIVE_INFINITY);
+    }
+}
